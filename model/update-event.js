00cceee2747fb67020eee787cf2950ef410151f7
knockknockyoo <iheaven0129@gmail.com>
2023-02-01 16:45:42 -0500

    Create static method - updated by app - api updated by admin user

--- model/update-event.js
+++ model/update-event.js
@@ -2,8 +2,7 @@ const {BentoEvent} = require("./bento-event");
 const {UPDATE} = require("../const/event-types");
 
 const UpdateEvent = class extends BentoEvent{
-    constructor(updatedField, oldValue, newValue, actingUserID, actingUserEmail, actingUserIDP,
-                updatedUserID, updatedUserEmail, updatedUserIDP) {
+    constructor(updatedField, oldValue, newValue, actingUserID, actingUserEmail, actingUserIDP) {
         super();
         this.event_type = UPDATE;
         this.updated_field = updatedField;
@@ -12,9 +11,19 @@ const UpdateEvent = class extends BentoEvent{
         this.acting_user_id = actingUserID;
         this.acting_user_email = actingUserEmail;
         this.acting_user_idp = actingUserIDP;
-        this.updated_user_id = updatedUserID;
-        this.updated_user_email = updatedUserEmail;
-        this.updated_user_idp = updatedUserIDP;
+    }
+
+    static updateUserEvent(updatedField, oldValue, newValue, actingUserID, actingUserEmail, actingUserIDP,
+                           updatedUserID, updatedUserEmail, updatedUserIDP) {
+        let event = new UpdateEvent(updatedField, oldValue, newValue, actingUserID, actingUserEmail, actingUserIDP);
+        event.updated_user_id = updatedUserID;
+        event.updated_user_email = updatedUserEmail;
+        event.updated_user_idp = updatedUserIDP;
+        return event;
+    }
+
+    static updateUserEventByApp(updatedField, oldValue, newValue, actingUserID, actingUserEmail, actingUserIDP) {
+        return new UpdateEvent(updatedField, oldValue, newValue, actingUserID, actingUserEmail, actingUserIDP);
     }
 }
 
