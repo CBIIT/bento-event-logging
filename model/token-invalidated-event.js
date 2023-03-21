5b2d9b2aba37e27dbf091bd58a8701ee040ea886
knockknockyoo <iheaven0129@gmail.com>
2023-03-21 11:37:15 -0400

    changed token_invalidated event

--- model/token-invalidated-event.js
+++ model/token-invalidated-event.js
@@ -1,10 +1,10 @@
 const {SingleUserEvent} = require("./single-user-event");
-const {TOKEN_CREATED} = require("../const/event-types");
+const {TOKEN_INVALIDATED} = require("../const/event-types");
 
 const TokenInvalidatedEvent = class extends SingleUserEvent{
     constructor(user, token) {
         super(user.ID, user.email, user.IDP);
-        this.event_type = TOKEN_CREATED;
+        this.event_type = TOKEN_INVALIDATED;
         this.token_uuid = token.uuid;
         this.token_expiration = token.expiration;
     };
