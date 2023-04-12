931b5f00623cf055c30152bb4d2196b891c35aba
knockknockyoo <iheaven0129@gmail.com>
2023-04-12 16:47:21 -0400

    Updated Token UUID Events

--- model/token-invalidated-event.js
+++ model/token-invalidated-event.js
@@ -2,10 +2,10 @@ const {SingleUserEvent} = require("./single-user-event");
 const {TOKEN_INVALIDATED} = require("../const/event-types");
 
 const TokenInvalidatedEvent = class extends SingleUserEvent{
-    constructor(user, token) {
+    constructor(user, tokenUUIDs) {
         super(user.ID, user.email, user.IDP);
         this.event_type = TOKEN_INVALIDATED;
-        this.token_uuid = token.uuid;
+        this.token_uuids = tokenUUIDs;
     };
 }
 
